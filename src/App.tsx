f0d50b7e8852447146e9133c1259648775117750
ploy-destroyer <ploy_destroyer@protonmail.com>
2023-02-07 13:29:24 +0000

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.scss';
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import Layout from './components/Layout';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* <Route path="/portfolio" element={<Portfolio />} /> */}
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        </Route>
+      </Routes>
     </div>
   )
 }
